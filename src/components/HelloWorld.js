a79f8efbb17e2525ccec699706415548882eed5a
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-08 13:33:06 +0900

    Create a new component

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './HelloWorld.css';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="HelloWorld">
+        <h1>Welcome to React</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
