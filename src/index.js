f41da5d732fd36695593ff5789a43ec66fd63ace
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-08 13:33:06 +0900

    Create a new component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const mountElement = document.getElementById('root');
 
-ReactDOM.render(<h1>Hello, world!</h1>, mountElement);
+ReactDOM.render(<HelloWorld />, mountElement);
 
 registerServiceWorker();
