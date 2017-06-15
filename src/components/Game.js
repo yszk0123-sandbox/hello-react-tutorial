0d09d0337da445362d512f6021c58cac8142cf31
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-15 14:53:12 +0900

    Show the moves

--- src/components/Game.js
+++ src/components/Game.js
@@ -57,6 +57,16 @@ export default class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ? `Move #${move}` : 'Game start';
+
+      return (
+        <li>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = `Winner: ${winner}`;
@@ -71,7 +81,7 @@ export default class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
