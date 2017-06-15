6e7ef0b64a4ee236190d102326b2e6c4baf03ac1
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-15 14:47:38 +0900

    Pass data through props

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,7 @@ export default class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
