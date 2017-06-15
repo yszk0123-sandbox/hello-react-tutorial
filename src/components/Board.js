1a897dad7146d7ce6b3645ac6c0b866db60066c1
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-15 15:03:09 +0900

    Lift state up

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,8 +2,26 @@ import React from 'react';
 import Square from './Square';
 
 export default class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
 export default class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
