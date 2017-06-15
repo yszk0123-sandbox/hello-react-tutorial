73176410a7419b4a2170d477498655a1451bb0e6
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-15 14:48:14 +0900

    Check how to make the Square component interactive

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
