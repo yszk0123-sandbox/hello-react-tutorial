745a231d38ba67bd1d1d146ff7277aefcb8b46a7
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-20 19:49:40 +0900

    Remove unused files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const mountElement = document.getElementById('root');
 
 ReactDOM.render(<App />, mountElement);
-
-registerServiceWorker();
